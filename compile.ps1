1b112d892696cbdb521476328429abd4f37b133f
Yuxuan Liu <116528196+alpha-liu-01@users.noreply.github.com>
2025-07-23 18:02:03 -0400

    Remove absolute paths

--- compile.ps1
+++ compile.ps1
@@ -6,7 +6,7 @@ mkdir build
 & "C:\Qt\6.8.2\mingw_64\bin\lrelease.exe" ./resources/translations/app_zh.ts ./resources/translations/app_fr.ts ./resources/translations/app_es.ts
 
 
-Copy-Item -Path "C:\Games\notecpp_2rowlowres_legacy\resources\translations\*.qm" -Destination "C:\Games\notecpp_2rowlowres_legacy\build" -Force
+Copy-Item -Path ".\resources\translations\*.qm" -Destination ".\build" -Force
 
 cd .\build
 cmake -G "MinGW Makefiles" .. 
@@ -18,7 +18,7 @@ cmake --build . -- -j16
 # ✅ Deploy Qt runtime
 & "C:\Qt\6.8.2\mingw_64\bin\windeployqt.exe" "NoteApp.exe"
 
-Copy-Item -Path "C:\Games\notecpp_2rowlowres\dllpack\*.dll" -Destination "C:\Games\notecpp_2rowlowres_legacy\build" -Force
-Copy-Item -Path "C:\Games\notecpp_2rowlowres_legacy\dllpack\bsdtar.exe" -Destination "C:\Games\notecpp_2rowlowres_legacy\build" -Force
+Copy-Item -Path "..\dllpack\*.dll" -Destination "..\build" -Force
+Copy-Item -Path "..\dllpack\bsdtar.exe" -Destination "..\build" -Force
 ./NoteApp.exe
 cd ../
